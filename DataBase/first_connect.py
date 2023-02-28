6940866f72cf3e28103e69fd408578961dfdad78
avjustice <iatsenko.av@gmail.com>
2023-03-01 00:46:42 +0300

    connecting to postgres on railway

--- DataBase/first_connect.py
+++ DataBase/first_connect.py
@@ -1,13 +1,19 @@
 import psycopg2
-from db_password import password
+from db_password import password_railway
 
-conn = psycopg2.connect(f"dbname='zamhrork' user='zamhrork' host='mouse.db.elephantsql.com' password={password}")
+conn = psycopg2.connect(f"dbname='railway' user='postgres' "
+                        f"port=5522 host='containers-us-west-91.railway.app' password={password_railway}")
 print(conn)
 cur = conn.cursor()
 insert_query = """ INSERT INTO shop VALUES (%s,%s)"""
-for i in range(3, 1000):
-    cur.execute(insert_query, (i, 'a' * (i % 10)))
-conn.commit()
+select_query = """SELECT * FROM shop WHERE shop_id % 13 = 0"""
+cur.execute(select_query)
+records = cur.fetchall()
+print(records)
+# for i in range(1000, 1100):
+#     cur.execute(insert_query, (i, 'a' * (i % 10)))
+# conn.commit()
+# print(i)
 cur.close()
 conn.close()
-print(i)
+# print(i)
--- DataBase/making_tables.py
+++ DataBase/making_tables.py
@@ -1,10 +1,12 @@
-"""CREATE TABLE public.shop
+"""CREATE TABLE IF NOT EXISTS public.shop
 (
     shop_id integer NOT NULL,
-    shop_name character varying NOT NULL,
-    PRIMARY KEY (shop_id)
-);
+    shop_name character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT shop_pkey PRIMARY KEY (shop_id)
+)
+
+TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.shop
-    OWNER to avjustice;"""
+    OWNER to postgres;"""""
 
