19460c4a671dbb922d6bd56ab5ef56f1874c90e8
Bereznikov <aleksey.bereznikov@gmail.com>
2023-03-17 23:04:14 +0300

    bag fix

--- Parsing/next.py
+++ Parsing/next.py
@@ -13,6 +13,7 @@ class Parser:
         self._section_names = ['Девочки', 'Мальчики', 'Для малышей', 'Женщины', "Мужчины", "Для дома"]
         self.result = []
         self.id_set = set()
+        self.result
     @staticmethod
     def making_soup_txt(url):
         ua = UserAgent()
@@ -93,69 +94,66 @@ class Parser:
         return res
 
     async def get_data(self, session, info_pair, counter):
-        result = []
         section_name = self._section_names[counter]
         category_name = info_pair[0]
         url = info_pair[1]
         for i in range(1, 826):
-                pagen_url = url.strip('-0') + '?p=' + str(i)
-                async with session.get(url=pagen_url) as response:
-                    resp = await response.text()
-                    page_soup = BeautifulSoup(resp, 'lxml')
-                    items = page_soup.find('div',
-                                           "MuiGrid-root MuiGrid-container plp-product-grid-wrapper plp-1s9f1m4").find_all(
-                        'div', class_="MuiCardContent-root produc-1ivfcou")
-                    if items:
-                        for item in items:
-                            name_id, price, *tale = item.find('a')['aria-label'].split(' | ')
-                            good_name = name_id[:name_id.find(' (')]
-                            good_id = name_id.rstrip(')').lstrip(' (')[-6:]
-                            good_link = item.find('a')['href']
-                            previous_sib = item.previous_sibling
-                            image = previous_sib.find('img')['src']
-                            if '-' in price or '&' in good_id:
-                                try:
-                                    price_low = price.split(' - ')[0].strip(' тг').replace(' ', '')
-                                    price_big = price.split(' - ')[1].strip(' тг').replace(' ', '')
-                                    good_dict = {'id': good_id,
-                                                'name': good_name,
-                                                'price_low': price_low,
-                                                'price_big': price_big,
-                                                'link': good_link,
-                                                'image_path': image,
-                                                'availability': 'in_stock',
-                                                'section_name': section_name,
-                                                'category_name': category_name}
-                                except:
-                                    brand, name_id, price, *tale = item.find('a')['aria-label'].split(' | ')
-                                    good_name = name_id[:name_id.find(' (')]
-                                    good_id = name_id.rstrip(')').lstrip(' (')[-6:]
-                                    good_dict = {'id': good_id,
-                                                'name': good_name,
-                                                'price_low': price.strip('тг').replace(' ', ''),
-                                                'price_big': price.strip('тг').replace(' ', ''),
-                                                'link': good_link, 'image_path': image,
-                                                'availability': 'in_stock',
-                                                'section_name': section_name,
-                                                'category_name': category_name}
-                            else:
+            pagen_url = url.strip('-0') + '?p=' + str(i)
+            async with session.get(url=pagen_url) as response:
+                resp = await response.text()
+                page_soup = BeautifulSoup(resp, 'lxml')
+                items = page_soup.find('div',
+                                       "MuiGrid-root MuiGrid-container plp-product-grid-wrapper plp-1s9f1m4").find_all(
+                    'div', class_="MuiCardContent-root produc-1ivfcou")
+                if items:
+                    for item in items:
+                        name_id, price, *tale = item.find('a')['aria-label'].split(' | ')
+                        good_name = name_id[:name_id.find(' (')]
+                        good_id = name_id.rstrip(')').lstrip(' (')[-6:]
+                        good_link = item.find('a')['href']
+                        previous_sib = item.previous_sibling
+                        image = previous_sib.find('img')['src']
+                        if '-' in price or '&' in good_id:
+                            try:
+                                price_low = price.split(' - ')[0].strip(' тг').replace(' ', '')
+                                price_big = price.split(' - ')[1].strip(' тг').replace(' ', '')
                                 good_dict = {'id': good_id,
-                                            'name': good_name,
-                                            'price_low': price.strip('тг').replace(' ', ''),
-                                            'price_big': price.strip('тг').replace(' ', ''),
-                                            'link': good_link,
-                                            'image_path': image, 'availability': 'in_stock',
-                                            'section_name': section_name,
-                                            'category_name': category_name}
-                            if good_dict['id'] not in self.id_set:
-                                result.append(good_dict)
-                                self.id_set.add(good_dict['id'])
-                            else:
-                                continue
-                        with open('next.json', 'a', encoding='utf-8') as file:
-                            json.dump(result, file, indent=4, ensure_ascii=False)
-                    else:
-                        break
+                                             'name': good_name,
+                                             'price_low': price_low,
+                                             'price_big': price_big,
+                                             'link': good_link,
+                                             'image_path': image,
+                                             'availability': 'in_stock',
+                                             'section_name': section_name,
+                                             'category_name': category_name}
+                            except:
+                                brand, name_id, price, *tale = item.find('a')['aria-label'].split(' | ')
+                                good_name = name_id[:name_id.find(' (')]
+                                good_id = name_id.rstrip(')').lstrip(' (')[-6:]
+                                good_dict = {'id': good_id,
+                                             'name': good_name,
+                                             'price_low': price.strip('тг').replace(' ', ''),
+                                             'price_big': price.strip('тг').replace(' ', ''),
+                                             'link': good_link, 'image_path': image,
+                                             'availability': 'in_stock',
+                                             'section_name': section_name,
+                                             'category_name': category_name}
+                        else:
+                            good_dict = {'id': good_id,
+                                         'name': good_name,
+                                         'price_low': price.strip('тг').replace(' ', ''),
+                                         'price_big': price.strip('тг').replace(' ', ''),
+                                         'link': good_link,
+                                         'image_path': image, 'availability': 'in_stock',
+                                         'section_name': section_name,
+                                         'category_name': category_name}
+                        if good_dict['id'] not in self.id_set:
+                            self.result.append(good_dict)
+                            self.id_set.add(good_dict['id'])
+                        else:
+                            continue
+                else:
+                    break
 
 
     async def main(self):
@@ -171,13 +169,15 @@ class Parser:
                     tasks.append(task)
                 counter += 1
             await asyncio.gather(*tasks)
-
+        with open('next.json', 'w', encoding='utf-8') as file:
+            json.dump(self.result, file, indent=4, ensure_ascii=False)
     def __call__(self, url, *args, **kwargs):
         soup = self.making_soup_txt(url)
         sections = self.get_url_sections(soup)
         self._section_links = sections
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
         asyncio.run(self.main())
+
 
 def make_next_json_with_category_id():
     with open('next.json', 'r', encoding='utf-8') as file:
@@ -186,7 +186,7 @@ def make_next_json_with_category_id():
         next_categories = json.load(file)
     next_categories_dict = {}
     for category in next_categories:
-        next_categories_dict[category['subcategory'], category['category']] = category['id']
+        next_categories_dict[category['category_name'], category['section_name']] = category['id']
     for product in next_product:
         if next_categories_dict.get((product['category_name'].upper(), product['section_name'].upper())):
             product['category_id'] =\
@@ -198,143 +198,3 @@ if __name__ == '__main__':
     parse_site = Parser()
     parse_site('https://www.nextdirect.com/kz/ru')
     make_next_json_with_category_id()
-
-# urlmain = 'https://www.nextdirect.com/kz/ru'
-# soupmain = makingsouptxt(urlmain)
-# primecategories = [f'{link["href"]}' for link in soupmain.find('ul', id='snail-trail-container').find_all('a')]
-# primecategories_names = [link.find('div', class_="header-5qe5c1").text for link in
-#                          soupmain.find('ul', id='snail-trail-container').find_all('a')]
-# primecounter = 0
-# del primecategories[5]
-# del primecategories_names[5]
-# res = []
-# id_list = []
-# id_set = set()
-# for primelink in primecategories[:-3]:
-#     url = primelink
-#     soup = makingsouptxt(url)
-#     primecategory_name = primecategories_names[primecounter]
-#     primecounter += 1
-#     categories_links = []
-#     categories_names = []
-#     if primelink == 'https://www.nextdirect.com/kz/ru/women':
-#         for link in soup.find_all('div', class_="sidebar-sec col-sm-12 col-md-3 col-lg-12"):
-#             women_links = link.find_all('a')
-#             for women_link in women_links:
-#                 if 'Все Дамское белье' in women_link.text:
-#                     categories_links.append(f'{url[:33]}{women_link["href"][7:]}')
-#                 elif ('Вся ' in women_link.text) or ('Все ' in women_link.text) or ('Новинки' in women_link.text) or (
-#                     'Новые ' in women_link.text) or ('Всё ' in women_link.text):
-#                     continue
-#                 else:
-#                     categories_links.append(f'{url[:33]}{women_link["href"][7:]}')
-#             for women_link in women_links:
-#                 if 'Все Дамское белье' in women_link.text:
-#                     categories_names.append(women_link.text)
-#                 elif ('Вся ' in women_link.text) or ('Все ' in women_link.text) or ('Новинки' in women_link.text) or (
-#                     'Новые ' in women_link.text) or ('Всё ' in women_link.text):
-#                     continue
-#                 else:
-#                     categories_names.append(women_link.text)
-#         counter = 0
-#         categories_links = categories_links[:15] + [categories_links[28]] + categories_links[31:]
-#         categories_names = categories_names[:15] + [categories_names[28]] + categories_names[31:]
-#     elif primelink == 'https://www.nextdirect.com/kz/ru/baby':
-#         link_list = soup.find_all('div', class_='row tablet-rows')[1]
-#         for link in link_list.find_all('a'):
-#             if ('Вся ' in link.text) or ('Все ' in link.text) or ('Новинки' in link.text) or (
-#                     'Новые ' in link.text) or ('Всё ' in link.text):
-#                 continue
-#             else:
-#                 categories_links.append(f'{url[:33]}{link["href"][7:]}')
-#         for link in link_list.find_all('a'):
-#             if ('Вся ' in link.text) or ('Все ' in link.text) or ('Новинки' in link.text) or (
-#                     'Новые ' in link.text) or ('Всё ' in link.text):
-#                 continue
-#             else:
-#                 categories_names.append(link.text)
-#         counter = 0
-#         categories_links = categories_links[-6:]
-#         categories_names = categories_names[-6:]
-#     else:
-#         for link in soup.find('div', class_='row tablet-rows').find_all('a'):
-#             if ('Вся ' in link.text) or ('Все ' in link.text) or ('Новинки' in link.text) or (
-#                     'Новые ' in link.text) or ('Всё ' in link.text):
-#                 continue
-#             else:
-#                 categories_links.append(f'{url[:33]}{link["href"][7:]}')
-#         for link in soup.find('div', class_='row tablet-rows').find_all('a'):
-#             if ('Вся ' in link.text) or ('Все ' in link.text) or ('Новинки' in link.text) or (
-#                     'Новые ' in link.text) or ('Всё ' in link.text):
-#                 continue
-#             else:
-#                 categories_names.append(link.text)
-#         counter = 0
-#     for link in categories_links:             ----------------------------------------------------------------------------
-#         category_name = categories_names[counter]
-#         counter += 1
-#         for i in range(1, 826):
-#             category_url = link.strip('-0') + '?p=' + str(i)
-#             item_soup = makingsouptxt(category_url)
-#             print(category_url)
-#             items = item_soup.find('div', "MuiGrid-root MuiGrid-container plp-product-grid-wrapper plp-1s9f1m4").find_all(
-#                 'div', class_="MuiCardContent-root produc-1ivfcou")
-#             if items:
-#                 for item in items:
-#                     nameid, price, *tale = item.find('a')['aria-label'].split(' | ')
-#                     good_name = nameid[:nameid.find(' (')]
-#                     good_id = nameid.rstrip(')').lstrip(' (')[-6:]
-#                     good_link = item.find('a')['href']
-#                     previoussib = item.previous_sibling
-#                     image = previoussib.find('img')['src']
-#                     if '-' in price or '&' in good_id:
-#                         try:
-#                             price_low = price.split(' - ')[0].strip(' тг').replace(' ', '')
-#                             price_big = price.split(' - ')[1].strip(' тг').replace(' ', '')
-#                             gooddict = {'id': good_id,
-#                                         'name': good_name,
-#                                         'price_low': price_low,
-#                                         'price_big': price_big,
-#                                         'link': good_link,
-#                                         'image_path': image,
-#                                         'availability': 'in_stock',
-#                                         'primecategory_name': primecategory_name,
-#                                         'category_name': category_name}
-#                         except:
-#                             brand, nameid, price, *tale = item.find('a')['aria-label'].split(' | ')
-#                             good_name = nameid[:nameid.find(' (')]
-#                             good_id = nameid.rstrip(')').lstrip(' (')[-6:]
-#                             gooddict = {'id': good_id,
-#                                         'name': good_name,
-#                                         'price_low': price.strip('тг').replace(' ', ''),
-#                                         'price_big': price.strip('тг').replace(' ', ''),
-#                                         'link': good_link, 'image_path': image,
-#                                         'availability': 'in_stock',
-#                                         'primecategory_name': primecategory_name,
-#                                         'category_name': category_name}
-#                     else:
-#                         gooddict = {'id': good_id,
-#                                     'name': good_name,
-#                                     'price_low': price.strip('тг').replace(' ', ''),
-#                                     'price_big': price.strip('тг').replace(' ', ''),
-#                                     'link': good_link,
-#                                     'image_path': image, 'availability': 'in_stock',
-#                                     'primecategory_name': primecategory_name,
-#                                     'category_name': category_name}
-#                     res.append(gooddict)
-#                     id_list.append(gooddict['id'])
-#                     id_set.add(gooddict['id'])
-#             else:
-#                 break
-# with open('next.json', 'w', encoding='utf-8') as file:
-#     json.dump(res, file, indent=4, ensure_ascii=False)
-# with open('next_id.txt', 'w', encoding='utf-8') as file1:
-#     for i in id_list:
-#         file1.write(i + '\n')
-# with open('next_idset.txt', 'w', encoding='utf-8') as file2:
-#     for j in id_set:
-#         file2.write(j + '\n')
-# if len(id_set) == len(id_list):
-#     print("Correct id's")
-# else:
-#     print('There some repeats in id')
--- Parsing/tests.py
+++ Parsing/tests.py
@@ -1,149 +1,38 @@
-from fake_useragent import UserAgent
-import requests
-import time
-from bs4 import BeautifulSoup
 import json
 
+result = list()
+result_set = set()
+with open('next.json', 'r', encoding='utf-8') as file_next, open('zara_categories.json', 'r', encoding='utf-8') as file_zara:
+    zara = file_zara.read()
+    zara_dicts = json.loads(zara)
+    content = file_next.read()
+    next_dicts = json.loads(content)
+    next_id = 1
+    for item_next in next_dicts:
+        for item_zara in zara_dicts:
+            if item_next['category_name'].lower() == item_zara['subcategory'].lower() and item_next['primecategory_name'].lower() == item_zara['category'].lower():
+                temp_set = (item_next['primecategory_name'].upper(), item_next['category_name'].upper())
+                if temp_set not in result_set:
+                    category_id = item_zara['id']
+                    temp = {"category": item_next['primecategory_name'].upper(),
+                            "subcategory": item_next['category_name'].upper(),
+                            "id": category_id}
+                    result_set.add(temp_set)
+                    print(temp)
+                    result.append(temp)
+            else:
+                temp_set = (item_next['primecategory_name'].upper(), item_next['category_name'].upper())
+                if temp_set not in result_set:
+                    space = '0' * (4 - len(str(next_id)))
+                    temp = {"category": item_next['primecategory_name'].upper(),
+                            "subcategory": item_next['category_name'].upper(),
+                            "id": 'NXT' + space + str(next_id)}
+                    result_set.add(temp_set)
+                    print(temp)
+                    result.append(temp)
+                    next_id += 1
+with open('next_categories.json', 'w', encoding='utf-8') as res_file:
+        json.dump(result, res_file, indent=4, ensure_ascii=False)
+print('end')
 
-def makingsouptxt(urldef):
-    ua = UserAgent()
-    fake_ua = {'user-agent': ua.random}
-    response = requests.get(url=urldef, headers=fake_ua)
-    response.encoding = 'utf-8'
-    return BeautifulSoup(response.text, 'lxml')
 
-
-urlmain = 'https://www.nextdirect.com/kz/ru'
-soupmain = makingsouptxt(urlmain)
-primecategories = [f'{link["href"]}' for link in soupmain.find('ul', id='snail-trail-container').find_all('a')]
-primecategories_names = [link.find('div', class_="header-5qe5c1").text for link in
-                         soupmain.find('ul', id='snail-trail-container').find_all('a')]
-primecounter = 0
-del primecategories[5]
-del primecategories_names[5]
-res = []
-id_list = []
-id_set = set()
-primecategories = ['https://www.nextdirect.com/kz/ru/women']
-for primelink in primecategories:
-    url = primelink
-    soup = makingsouptxt(url)
-    primecategory_name = primecategories_names[primecounter]
-    primecounter += 1
-    # categories_links = [f'{url[:33]}{link["href"][7:]}' for link in
-    #                     soup.find('div', class_='row tablet-rows').find_all('a') if
-    #                     'Вся ' not in link.text and 'Все ' not in link.text and 'Новинки' not in link.text]
-    categories_links = []
-    # categories_names = [link.text for link in soup.find('div', class_='row tablet-rows').find_all('a') if
-    #                     'Вся' not in link.text and 'Все' not in link.text and 'Новинки' not in link.text]
-    categories_names = []
-    if primelink == 'https://www.nextdirect.com/kz/ru/women':
-        for link in soup.find_all('div', class_="sidebar-sec col-sm-12 col-md-3 col-lg-12"):
-            women_links = link.find_all('a')
-            for women_link in women_links:
-                if 'Все Дамское белье' in women_link.text:
-                    categories_links.append(f'{url[:33]}{women_link["href"][7:]}')
-                elif ('Вся ' in women_link.text) or ('Все ' in women_link.text) or ('Новинки' in women_link.text) or (
-                    'Новые ' in women_link.text) or ('Всё ' in women_link.text):
-                    continue
-                else:
-                    categories_links.append(f'{url[:33]}{women_link["href"][7:]}')
-            for women_link in women_links:
-                if 'Все Дамское белье' in women_link.text:
-                    categories_names.append(women_link.text)
-                elif ('Вся ' in women_link.text) or ('Все ' in women_link.text) or ('Новинки' in women_link.text) or (
-                    'Новые ' in women_link.text) or ('Всё ' in women_link.text):
-                    continue
-                else:
-                    categories_names.append(women_link.text)
-        counter = 0
-        categories_links = categories_links[:15] + [categories_links[28]] + categories_links[31:]
-        categories_names = categories_names[:15] + [categories_names[28]] + categories_names[31:]
-    elif primelink == 'https://www.nextdirect.com/kz/ru/baby':
-        link_list = soup.find_all('div', class_='row tablet-rows')[1]
-        for link in link_list.find_all('a'):
-            if ('Вся ' in link.text) or ('Все ' in link.text) or ('Новинки' in link.text) or (
-                    'Новые ' in link.text) or ('Всё ' in link.text):
-                continue
-            else:
-                categories_links.append(f'{url[:33]}{link["href"][7:]}')
-        for link in link_list.find_all('a'):
-            if ('Вся ' in link.text) or ('Все ' in link.text) or ('Новинки' in link.text) or (
-                    'Новые ' in link.text) or ('Всё ' in link.text):
-                continue
-            else:
-                categories_names.append(link.text)
-        counter = 0
-        categories_links = categories_links[-6:]
-        categories_names = categories_names[-6:]
-    else:
-        for link in soup.find('div', class_='row tablet-rows').find_all('a'):
-            if ('Вся ' in link.text) or ('Все ' in link.text) or ('Новинки' in link.text) or (
-                    'Новые ' in link.text) or ('Всё ' in link.text):
-                continue
-            else:
-                categories_links.append(f'{url[:33]}{link["href"][7:]}')
-        for link in soup.find('div', class_='row tablet-rows').find_all('a'):
-            if ('Вся ' in link.text) or ('Все ' in link.text) or ('Новинки' in link.text) or (
-                    'Новые ' in link.text) or ('Всё ' in link.text):
-                continue
-            else:
-                categories_names.append(link.text)
-        counter = 0
-    for link in categories_links:
-        category_name = categories_names[counter]
-        counter += 1
-        for i in range(1, 826):
-            category_url = link.strip('-0') + '?p=' + str(i)
-            item_soup = makingsouptxt(category_url)
-            print(category_url)
-            items = item_soup.find('div', "MuiGrid-root MuiGrid-container plp-product-grid-wrapper plp-1s9f1m4").find_all(
-                'div', class_="MuiCardContent-root produc-1ivfcou")
-            if items:
-                for item in items:
-                    nameid, price, *tale = item.find('a')['aria-label'].split(' | ')
-                    good_name = nameid[:nameid.find(' (')]
-                    good_id = nameid.rstrip(')').lstrip(' (')[-6:]
-                    good_link = item.find('a')['href']
-                    previoussib = item.previous_sibling
-                    image = previoussib.find('img')['src']
-                    if '-' in price or '&' in good_id:
-                        try:
-                            price_low = price.split(' - ')[0].strip(' тг').replace(' ', '')
-                            price_big = price.split(' - ')[1].strip(' тг').replace(' ', '')
-                            gooddict = {'id': good_id,
-                                        'name': good_name,
-                                        'price_low': price_low,
-                                        'price_big': price_big,
-                                        'link': good_link,
-                                        'image_path': image,
-                                        'availability': 'in_stock',
-                                        'primecategory_name': primecategory_name,
-                                        'category_name': category_name}
-                        except:
-                            brand, nameid, price, *tale = item.find('a')['aria-label'].split(' | ')
-                            good_name = nameid[:nameid.find(' (')]
-                            good_id = nameid.rstrip(')').lstrip(' (')[-6:]
-                            gooddict = {'id': good_id,
-                                        'name': good_name,
-                                        'price_low': price.strip('тг').replace(' ', ''),
-                                        'price_big': price.strip('тг').replace(' ', ''),
-                                        'link': good_link, 'image_path': image,
-                                        'availability': 'in_stock',
-                                        'primecategory_name': primecategory_name,
-                                        'category_name': category_name}
-                    else:
-                        gooddict = {'id': good_id,
-                                    'name': good_name,
-                                    'price_low': price.strip('тг').replace(' ', ''),
-                                    'price_big': price.strip('тг').replace(' ', ''),
-                                    'link': good_link,
-                                    'image_path': image, 'availability': 'in_stock',
-                                    'primecategory_name': primecategory_name,
-                                    'category_name': category_name}
-                    res.append(gooddict)
-                    print(gooddict)
-            else:
-                break
-with open('test.json', 'w', encoding='utf-8') as file:
-    json.dump(res, file, indent=4, ensure_ascii=False)
