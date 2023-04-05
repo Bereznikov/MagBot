9f8753bb464d447b4c5db7233af343cfee3266a9
avjustice <iatsenko.av@gmail.com>
2023-04-05 21:54:35 +0300

    Deleted commented code

--- magbot/magBot.py
+++ magbot/magBot.py
@@ -440,21 +440,6 @@ async def checkout(update, context):
 
 async def error_handler(update, context):
     logger.error(msg="Exception while handling an update:", exc_info=context.error)
-    # tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
-    # tb_string = "".join(tb_list)
-    #
-    # update_str = update.to_dict() if isinstance(update, Update) else str(update)
-    # message = (
-    #     f"An exception was raised while handling an update\n"
-    #     f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
-    #     "</pre>\n\n"
-    #     f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
-    #     f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
-    #     f"<pre>{html.escape(tb_string)}</pre>"
-    # )
-    # logger.error(msg=message)
-    # await context.bot.send_message(chat_id=106683136, text=message, parse_mode="HTML")
-
 
 if __name__ == '__main__':
     # my_persistence = PicklePersistence(filepath='my_file.pkl')
